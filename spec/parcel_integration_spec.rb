611012f954cb5b36c8c0a9c0bba18e3474470e4e
Halle Williams and Lauren Posey <halle@>
2016-07-05 15:33:07 -0700

    run integration spec

--- spec/parcel_integration_spec.rb
+++ spec/parcel_integration_spec.rb
@@ -1,4 +1,17 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('parcle path' , {:type => :feature}) do
+  it('processes the user entry and returns the cost of shipping') do
+    visit('/')
+    fill_in('length', :with=> 5)
+    fill_in('width', :with=> 5)
+    fill_in('height', :with=> 5)
+    fill_in('speed', :with=> 2)
+    fill_in('weight', :with=> 71)
+    click_button('submit')
+    expect(page).to have_content('65')
+  end
+end
