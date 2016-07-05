275c807b6305921560528b2098b32ee06115236e
Halle Williams and Lauren Posey <lauren@>
2016-07-05 15:48:38 -0700

    fetch data from forms to return our cost

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ require('./lib/parcel')
 get('/') do
   erb(:index)
 end
+
+get('/parcel') do
+  length = params.fetch('length').to_i
+  width = params.fetch('width').to_i
+  height = params.fetch('height').to_i
+  speed = params.fetch('speed').to_i
+  weight = params.fetch('weight').to_i
+
+  @parcel= Parcel.new(length, width, height, speed, weight)
+  erb(:parcel)
+end
